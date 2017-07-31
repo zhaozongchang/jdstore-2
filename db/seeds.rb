ff58e58b9464ed266df7794c3b315286f97051ca
zhaozongchang <xingyingsuifeng@gmail.com>
2017-07-31 16:28:52 +0800

    only nadmin can access backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u = User.new
+u.email = "865567799@qq.com"           # 可以改成自己的 email
+
+u.password = "111111"                # 最少要六码
+
+u.password_confirmation = "111111"   # 最少要六码
+
+u.is_admin = true
+u.save
+
+u = User.new
+u.email = "13188887761@163.com"           # 可以改成自己的 email
+
+u.password = "111111"                # 最少要六码
+
+u.password_confirmation = "111111"   # 最少要六码
+
+u.is_admin = true
+u.save
